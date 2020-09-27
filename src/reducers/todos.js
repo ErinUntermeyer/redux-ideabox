abb1e32345c6ea5ae801e6273894ccb97d0b3a6d
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-27 13:32:48 -0600

    Add reducer TOGGLE_TODO

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -7,6 +7,13 @@ export const todos = (state = [], action) => {
 	switch(action.type) {
 		case 'ADD_TODO': // this matches the type we declared in our action creator
 			return [...state, { id: Date.now(), todo: action.todo, completed: false }]
+		case 'TOGGLE_TODO':
+			return state.map(todo => {
+				if (action.id === todo.id) {
+					todo.completed = !todo.completed
+				}
+				return todo
+			})
 		default:
 			return state
 	}

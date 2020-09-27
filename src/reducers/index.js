973fd20c1878eb30ab476c982171c35ad880a69e
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-27 12:16:46 -0600

    Add notes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
+// creating the store
 import { combineReducers } from 'redux'
 import { todos } from './todos'
 
+// combineReducers is what combines all of our reducers, typically named rootReducer
 const rootReducer = combineReducers({
 	todos
 })

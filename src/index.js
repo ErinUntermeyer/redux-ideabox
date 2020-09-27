b485bb8fbaf59e536020d9423872ea0c2e0bead3
Erin Untermeyer <aerinuntermeyer@gmail.com>
2020-09-27 12:00:27 -0600

    Update App imports and createStore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux' // makes the Redux store available to any nested components. Usually rendered at the top level (likely App). If using Router, it wraps BrowserRouter.
+import { createStore } from 'redux' // creates a Redux store that holds the complete state tree of the app. There should only be a single store in the app.
+import { composeWithDevTools } from 'redux-devtools-extension' // can be passed as an argument with createStore so that we have access to the devtools. Order matters here.
+import rootReducer from './reducers'
+import App from './components/App'
+import './index.css'
+
+const store = createStore(rootReducer, composeWithDevTools())
 
 ReactDOM.render(
-  <App />,
+	<Provider store={store}>
+		<App />,
+	</Provider>,
   document.getElementById('root')
-);
+)
